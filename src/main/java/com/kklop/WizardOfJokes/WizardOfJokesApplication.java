ae27614c245fa3fe41a2ba306c7c2540d42c6928
Kevin Klopfenstein <klopfensteinsoftware@gmail.com>
2018-11-02 22:31:25 -0400

    - initial commit

---
+++ src/main/java/com/kklop/WizardOfJokes/WizardOfJokesApplication.java
@@ -0,0 +1,13 @@
+package com.kklop.WizardOfJokes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WizardOfJokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WizardOfJokesApplication.class, args);
+	}
+}
